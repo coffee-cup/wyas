45974116825ec5c301268a6e81954be4db7c6353
Jake Runzer <jakerunzer@gmail.com>
2017-12-26 01:29:44 -0700

    custom show for lispvals

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -9,4 +9,18 @@ data LispVal
   | String String
   | Bool Bool
   | Character Char
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
+
+instance Show LispVal where
+  show (String contents) = "\"" ++ contents ++ "\""
+  show (Atom name) = name
+  show (Number contents) = show contents
+  show (Float contents) = show contents
+  show (Bool True) = "#t"
+  show (Bool False) = "#f"
+  show (Character c) = show c
+  show (List contents) = "(" ++ unwordsList contents ++ ")"
+  show (DottedList h t) = "(" ++ unwordsList h ++ " . " ++ show t ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map show
