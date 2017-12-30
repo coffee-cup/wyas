2833c150419e5f647eb957c8a61a7d156405c58f
Jake Runzer <jakerunzer@gmail.com>
2017-12-30 02:11:01 -0700

    basic calculator

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,14 @@ import System.Console.Haskeline
 import Text.Megaparsec.Error
 
 import Parser
+import Eval
 
 process :: String -> IO ()
 process line = do
   let res = parseExpr line
   case res of
     Left err -> putStr $ parseErrorPretty err
-    Right ex -> print ex
+    Right ex -> print $ eval ex
 
 main :: IO ()
 main = runInputT defaultSettings loop
