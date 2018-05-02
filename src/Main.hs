7bc6dbee7c85ea68e72eec0edbe353b42a507004
Jake Runzer <jakerunzer@gmail.com>
2018-05-02 15:38:14 -0700

    cli

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,41 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
+import           Data.Text.IO        as TIO
 import           Eval
+import           Options.Applicative
+import           Repl
+import           System.Directory
 
-import           Control.Monad.Trans
-import qualified Data.Text                as T
-import           System.Console.Haskeline
+runScript :: FilePath -> IO ()
+runScript fname = do
+  exists <- doesFileExist fname
+  if exists
+    then TIO.readFile fname >>= evalFile fname
+    else TIO.putStrLn "File does not exists."
 
-type Repl a = InputT IO a
+data LineOpts = UseReplLineOpts | RunScriptLineOpts String
+
+parseLineOpts :: Parser LineOpts
+parseLineOpts = runScriptOpt <|> runReplOpt
+  where
+    runScriptOpt =
+      RunScriptLineOpts <$> strOption (long "script"
+                                       <> short 's'
+                                       <> metavar "SCRIPT"
+                                       <> help "File containing the script you want to run")
+    runReplOpt =
+      UseReplLineOpts <$ flag' () (long "repl"
+                                   <> short 'r'
+                                   <> help "Run as interavtive read/evaluate/print/loop")
+
+schemeEntryPoint :: LineOpts -> IO ()
+schemeEntryPoint UseReplLineOpts            = Repl.mainLoop --repl
+schemeEntryPoint (RunScriptLineOpts script) = runScript script
 
 main :: IO ()
-main = runInputT defaultSettings repl
-
-repl :: Repl ()
-repl = do
-  minput <- getInputLine "wyas> "
-  case minput of
-    Nothing -> outputStrLn "Goodbye."
-    Just input -> liftIO (process input) >> repl
-
-process :: String -> IO ()
-process str = do
-  res <- safeExec $ evalText $ T.pack str
-  either putStrLn return res
-
-processToAST :: String -> IO ()
-processToAST  str = print $ runParseTest $ T.pack str
+main = execParser opts >>= schemeEntryPoint
+  where
+    opts = info (helper <*> parseLineOpts)
+      ( fullDesc
+     <> header "Executable binary for Write You A Scheme v2.0"
+     <> progDesc "contains an entry point for both running scripts and repl" )
---
+++ src/Repl.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Repl where
+
+import           Eval
+
+import           Control.Monad.Trans
+import qualified Data.Text                as T
+import           System.Console.Haskeline
+
+type Repl a = InputT IO a
+
+mainLoop :: IO ()
+mainLoop = runInputT defaultSettings repl
+
+repl :: Repl ()
+repl = do
+  minput <- getInputLine "wyas> "
+  case minput of
+    Nothing -> outputStrLn "Goodbye."
+    Just input -> liftIO (process input) >> repl
+
+process :: String -> IO ()
+process str = do
+  res <- safeExec $ evalText $ T.pack str
+  either putStrLn return res
+
+processToAST :: String -> IO ()
+processToAST  str = print $ runParseTest $ T.pack str
